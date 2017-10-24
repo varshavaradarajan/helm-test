5cdf9433913cf9c85944e01ad46b6d01c6873fa5
Varsha Varadarajan <varshasvaradarajan@gmail.com>
2017-10-24 09:43:20 +0530

    Add machine type.

--- helm_tests_setup.rb
+++ helm_tests_setup.rb
@@ -19,13 +19,14 @@ cluster_name = env('CLUSTER_NAME')
 project_name = env('PROJECT_NAME')
 region = ENV['REGION'] || 'us-east1-b' # should be set up at the pipeline level
 release_name = 'gocd-helm-release'
+machine_type = ENV['MACHINE_TYPE'] || 'n1-standard-2'
 
 task :create_cluster do
   num_of_nodes = ENV['NUMBER_OF_NODES'] || 1
   disk_size = ENV['NODE_DISK_SIZE'] || 50
 
   sh("gcloud config set project #{project_name}")
-  sh("gcloud container clusters create #{cluster_name} --disk-size=#{disk_size} --num-nodes=#{num_of_nodes} --zone #{region} || true")
+  sh("gcloud container clusters create #{cluster_name} --disk-size=#{disk_size} --num-nodes=#{num_of_nodes} --zone #{region} --machine-type=#{machine_type} || true")
   sh("gcloud container clusters get-credentials #{cluster_name} --zone #{region}")
 end
 
